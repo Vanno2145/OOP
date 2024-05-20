562c3a507b6fe074fc64c3f64b838ebad2c49c3c
Ivan <ivanmlanar16@gmail.com>
2024-05-20 18:11:39 +0300

    Create calculator

--- OOP/FileName.cpp
+++ OOP/FileName.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
-int main() {
+int Plus(double a, double b) {
+	return a + b;
+}
 
+int Minus(double a, double b) {
+	return a - b;
+}
+
+int Division(double a, double b) {
+	return a / b;
+}
+
+int Multiple(double a, double b) {
+	return a * b;
+}
+
+int main() {
+	double a, b;
+	int choice;
+	cout << "1 - Minus; 2 - Plus; 3 - Multiple; 4 - Division";
+	cin >> choice;
+	cout << "First num: ";
+	cin >> a;
+	cout << "Second num: ";
+	cin >> b;
+
+	if (choice == 1) {
+		cout << Minus(a, b);
+	}
+	else if (choice == 2) {
+		cout << Plus(a, b);
+	}else if(choice == 3) {
+		cout << Multiple(a, b);
+	}
+	else if (choice == 4) {
+		if (b != 0) {
+			cout << Division(a, b);
+		}
+		else {
+			cout << "ERROR";
+		}
+	}
 }
