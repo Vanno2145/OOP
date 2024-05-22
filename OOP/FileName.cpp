22064bffa9fc8d782fd9635ff1bf89e78c819cf2
Ivan <ivanmlanar16@gmail.com>
2024-05-22 18:34:11 +0300

    Арифметические действия с дробями ООП

--- OOP/FileName.cpp
+++ OOP/FileName.cpp
@@ -2,46 +2,82 @@
 
 using namespace std;
 
-int Plus(double a, double b) {
-	return a + b;
-}
+class Drob {
+	int ch;
+	int zn;
+public:
+	Drob() {
+		cout << "Construct by default" << endl;
+		zn = ch = 1;
+	}
+	Drob(int a,  int b) {
+		cout << "Construct by default" << endl;
+		ch = a;
+		zn = b;
+	}
+	void Init() {
+		cout << "Input Denominator: ";
+		cin >> zn;
+		cout << "Input Numerator: ";
+		cin >> ch;
+	}
+	void Show() {
+		cout << ch << endl << "_" << endl << zn << endl;
+	}
+	Drob Plus(Drob & b) {
+		Drob rez;
 
-int Minus(double a, double b) {
-	return a - b;
-}
+		rez.ch = ch + b.ch;
+		rez.zn = zn + b.zn;
+		return rez;
+	}
+	Drob Minus(Drob& b) {
+		Drob rez;
 
-int Division(double a, double b) {
-	return a / b;
-}
+		rez.ch = ch - b.ch;
+		rez.zn = zn - b.zn;
+		return rez;
+	}
+	Drob Multiple(Drob& b) {
+		Drob rez;
 
-int Multiple(double a, double b) {
-	return a * b;
-}
+		rez.ch = ch * b.ch;
+		rez.zn = zn * b.zn;
+		return rez;
+	}
+	Drob Division(Drob& b) {
+		Drob rez;
+
+		rez.ch = ch / b.ch;
+		rez.zn = zn / b.zn;
+		return rez;
+	}
+
+};
 
 int main() {
-	double a, b;
+	Drob a;
+
+	a.Init();
+	Drob b;
+	b.Init();
+	Drob c;
+	
+
 	int choice;
-	cout << "1 - Minus; 2 - Plus; 3 - Multiple; 4 - Division";
+	cout << "Minus - 1; Plus - 2; Multiple - 3; Division - 4" << endl;
 	cin >> choice;
-	cout << "First num: ";
-	cin >> a;
-	cout << "Second num: ";
-	cin >> b;
 
 	if (choice == 1) {
-		cout << Minus(a, b);
+		c = a.Minus(b);
 	}
 	else if (choice == 2) {
-		cout << Plus(a, b);
-	}else if(choice == 3) {
-		cout << Multiple(a, b);
+		c = a.Plus(b);
+	}
+	else if (choice == 3) {
+		c = a.Multiple(b);
 	}
 	else if (choice == 4) {
-		if (b != 0) {
-			cout << Division(a, b);
-		}
-		else {
-			cout << "ERROR";
-		}
+		c = a.Division(b);
 	}
 }
